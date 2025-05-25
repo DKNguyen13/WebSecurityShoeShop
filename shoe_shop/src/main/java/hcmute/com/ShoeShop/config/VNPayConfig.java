801a550067a7df39a3eee49ddbdf749feb159ca4
DKNguyen13 <nd2004lk13@gmail.com>
2025-05-25 08:44:48 +0700

    fix bug

--- shoe_shop/src/main/java/hcmute/com/ShoeShop/config/VNPayConfig.java
+++ shoe_shop/src/main/java/hcmute/com/ShoeShop/config/VNPayConfig.java
@@ -11,7 +11,7 @@ import java.util.*;
 public class VNPayConfig {
 
     public static String vnp_PayUrl = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
-    public static String vnp_ReturnUrl = "http://localhost:8080/order/getPaymentInfo";
+    public static String vnp_ReturnUrl = "https://localhost:8443/order/getPaymentInfo";
     public static String vnp_TmnCode = "0S7T01T8";
     public static String secretKey = "BEZLUPOPOTXTDYZHCBGDJBHFJPBLSARL";
     public static String vnp_ApiUrl = "https://sandbox.vnpayment.vn/merchant_webapi/api/transaction";
--- shoe_shop/src/main/java/hcmute/com/ShoeShop/config/WebSecurityConfig.java
+++ shoe_shop/src/main/java/hcmute/com/ShoeShop/config/WebSecurityConfig.java
@@ -3,9 +3,12 @@ package hcmute.com.ShoeShop.config;
 import hcmute.com.ShoeShop.component.CustomAuthenticationSuccessHandler;
 import hcmute.com.ShoeShop.services.imp.CustomUserDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -16,6 +19,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.apache.catalina.connector.Connector;
+
 
 @Configuration
 @EnableWebSecurity
@@ -30,7 +35,10 @@ public class WebSecurityConfig {
 
         @Bean
         public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-                httpSecurity.authorizeHttpRequests(request -> request
+                httpSecurity
+                        .cors(AbstractHttpConfigurer::disable)
+                        .requiresChannel(channel -> channel.anyRequest().requiresSecure()) // bắt buộc HTTPS
+                        .authorizeHttpRequests(request -> request
                                 .requestMatchers("/admin/**").hasRole("admin")
                                 .requestMatchers("/manager/**").hasAnyRole("manager", "admin")
                                 .requestMatchers("/shipper/**").hasRole("shipper")
@@ -49,7 +57,12 @@ public class WebSecurityConfig {
                                 logout.logoutUrl("/logout").permitAll()
                         )
                         .exceptionHandling(exception -> exception
-                                .accessDeniedHandler(accessDeniedHandler())
+                                .accessDeniedHandler(accessDeniedHandler()))
+                        .headers(headers -> headers
+                                .httpStrictTransportSecurity(hsts -> hsts
+                                        .includeSubDomains(true)
+                                        .maxAgeInSeconds(31536000))
+                                .contentTypeOptions(Customizer.withDefaults())
                         );
                 //cai nay tu bat nen phai tat
                 //httpSecurity.csrf(AbstractHttpConfigurer::disable);
@@ -73,5 +86,20 @@ public class WebSecurityConfig {
                         // Chuyển hướng đến trang thông báo
                         response.sendRedirect("/access-denied");
                 };
+        }
+
+        @Bean
+        public ServletWebServerFactory servletContainer() {
+                TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+                tomcat.addAdditionalTomcatConnectors(redirectConnector());
+                return tomcat;
+        }
+        private Connector redirectConnector() {
+                Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+                connector.setScheme("http");
+                connector.setPort(8080); // Port HTTP
+                connector.setSecure(false);
+                connector.setRedirectPort(8443); // Redirect sang HTTPS port
+                return connector;
         }
 }
